fae42dedc0bd2e6fac49cdeba3eaebcc83f5b1e0
michaeljav <michaeljaviermota@gmail.com>
2025-04-29 16:23:49 -0400

    deleted file not need ed

--- nestjs_crud_app/src/app.service.ts
+++ nestjs_crud_app/src/app.service.ts
@@ -3,10 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World modificado2!';
+    return 'Hello World modificado23!';
   }
 
   getGreeting(name: string): string {
-    return `Hello ${name}!`;
+    return `Hello 3 ${name}!`;
   }
 }
